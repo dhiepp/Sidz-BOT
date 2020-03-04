9d8c7b66df538d8267a001c5478eaf3f8bf2c186
dhiep <dhiepp@users.noreply.github.com>
2020-03-04 15:46:10 +0700

    fix about cmd

--- commands/about.js
+++ commands/about.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
-const { client } = require('../index.js');
+const index = require('../index.js');
 
 module.exports = {
 	name: 'about',
@@ -8,7 +8,7 @@ module.exports = {
 	aliases: ['bot', 'sidz'],
 	cooldown: 10,
 	execute(message) {
-		const version = process.env.npm_package_version;
+		const client = index.client;
 		const uptime = process.uptime();
 		const days = Math.floor(uptime / 86400);
 		const hours = Math.floor((uptime % 86400) / 3600).toString().padStart(2, '0');
@@ -17,12 +17,12 @@ module.exports = {
 		const upString = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
 		const embed = new Discord.RichEmbed()
-			.setAuthor(`${config.bot_name} v${version}`, client.user.avatarURL)
+			.setAuthor(`${config.bot_name} v${config.version}`, client.user.avatarURL)
 			.setColor('AQUA')
 			.setTitle('Made by **Sidz**`#0242`')
 			.setDescription(`Prefix: \`${config.prefix}\`` + '\nCommands: `s.help`'
 				+ `\nUptime: **${upString}**\nServers: **${client.guilds.size}**`)
-			.addField('🗨️ Bot Discord', `[Click to join bot server](${config.invite_link})`)
+			.addField('🗨️ Bot Server', `[Click to join bot server](${config.server_link})`)
 			.addField('🤖 Invite Bot', `[Click to add to your server](${config.invite_link})`)
 			.setFooter(config.footer);
 
