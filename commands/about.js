038b653bd58ac46746c3d1da6171ed4b14d7c5b3
dhiep <dhiepp@users.noreply.github.com>
2020-03-04 15:30:43 +0700

    uptime format

--- commands/about.js
+++ commands/about.js
@@ -14,11 +14,11 @@ module.exports = {
 	execute(message) {
 		const version = process.env.npm_package_version;
 		const uptime = process.uptime();
-		const days = Math.round(uptime / 86400).padStart(2, '0');
-		const hours = Math.round((uptime % 86400) / 3600).padStart(2, '0');
-		const minutes = Math.round((uptime % 3600) / 60).padStart(2, '0');
-		const seconds = Math.round(uptime % 60).padStart(2, '0');
-		const upString = `${days}:${hours}:${minutes}:${seconds}`;
+		const days = Math.floor(uptime / 86400);
+		const hours = Math.floor((uptime % 86400) / 3600).padStart(2, '0');
+		const minutes = Math.floor((uptime % 3600) / 60).padStart(2, '0');
+		const seconds = Math.floor(uptime % 60).padStart(2, '0');
+		const upString = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
 		const embed = new Discord.RichEmbed()
 			.setAuthor(`${config.bot.name} v${version}`, client.user.avatarURL)
