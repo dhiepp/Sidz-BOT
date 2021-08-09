9e2f8f24162e74402e107075a2263a9d45efbee1
dhiep <dhonghiep@gmail.com>
2021-08-10 00:39:35 +0700

    modify some commands

--- commands/check.js
+++ commands/check.js
@@ -1,14 +1,16 @@
+const { developer_user_id } = require('../config.json');
 module.exports = {
 	name: 'check',
 	description: 'Perform check nubs for you',
 	cooldown: 10,
-	execute(message) {
-		const name = message.author.username;
-		message.channel.send(`:clock1:  Xin chờ, đang check nubs cho **${name}** ...`)
-			.then((loadMsg) => setTimeout(() => {
-				const ran = Math.round(Math.random());
-				if (name === 'Sidz' || ran == 0) loadMsg.edit(`:white_check_mark:  Xin chúc mừng, **${name}** không phải là nubs!`);
-				else loadMsg.edit(`:warning:  Cảnh báo! **${name}** chính là nubs!`);
-			}, 2000));
+	execute(interaction) {
+		const name = interaction.user.username;
+		interaction.reply(`:clock1:  Xin chờ, đang check nubs cho **${name}** ...`)
+		setTimeout(() => {
+			const ran = Math.round(Math.random());
+			if (interaction.user.id == developer_user_id || ran == 0) 
+				interaction.editReply(`:white_check_mark:  Xin chúc mừng, **${name}** không phải là nubs!`);
+			else interaction.editReply(`:warning:  Cảnh báo! **${name}** chính là nubs!`);
+		}, 3000);
 	},
 };
